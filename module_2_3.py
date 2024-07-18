7773f533fc84abd06f65864e019237b581896706
VladimirRomano <romanidze85@gmail.com>
2024-07-18 15:41:40 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,33 @@
+while 1 > 0:
+    my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+    n = 0
+    if my_list[0] >= n:
+        print(my_list[0])
+        if my_list[1] >= n:
+            print(my_list[1])
+            if my_list[2] >= n:
+                print(my_list[2])
+                if my_list[3] >= n:
+                    print(my_list[3])
+                    if my_list[4] >= n:
+                        print(my_list[4])
+                        if my_list[5] >= n:
+                            print(my_list[5])
+                            if my_list[6] >= n:
+                                print(my_list[6])
+                                if my_list[7] >= n:
+                                    print(my_list[7])
+                                    if my_list[8] >= n:
+                                        print(my_list[8])
+                                        if my_list[9] >= n:
+                                            print(my_list[9])
+                                            if my_list[10] >= n:
+                                                print(my_list[10])
+                                                if my_list[11] >= n:
+                                                    print(my_list[11])
+                                                    if my_list[12] >= n:
+                                                        print(my_list[12])
+
+                                                    else:
+                                                        print()
+                                                        break
