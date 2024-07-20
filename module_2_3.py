747ce6bedf43d20b49a5b4fccd51a0606c3b6b66
VladimirRomano <romanidze85@gmail.com>
2024-07-20 17:20:18 +0300

    Add files via upload

--- module_2_3.py
+++ module_2_3.py
@@ -1,32 +1,12 @@
-while 1 > 0:
-    my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-    n = 0
-    if my_list[0] >= n:
-        print(my_list[0])
-        if my_list[1] >= n:
-            print(my_list[1])
-            if my_list[2] >= n:
-                print(my_list[2])
-                if my_list[3] >= n:
-                    print(my_list[3])
-                    if my_list[4] >= n:
-                        print(my_list[4])
-                        if my_list[5] >= n:
-                            print(my_list[5])
-                            if my_list[6] >= n:
-                                print(my_list[6])
-                                if my_list[7] >= n:
-                                    print(my_list[7])
-                                    if my_list[8] >= n:
-                                        print(my_list[8])
-                                        if my_list[9] >= n:
-                                            print(my_list[9])
-                                            if my_list[10] >= n:
-                                                print(my_list[10])
-                                                if my_list[11] >= n:
-                                                    print(my_list[11])
-                                                    if my_list[12] >= n:
-                                                        print(my_list[12])
-                                break
-    else:
-        print()
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+new_list = []
+index = 0
+while index < len(my_list):
+    if my_list[index] < 0:
+        break
+    if my_list[index] == 0:
+        continue
+    if my_list[index] > 0:
+        new_list.append(my_list[index])
+    index += 1
+print(new_list)
